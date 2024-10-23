1322e8eb5fa881268ee14735473918b118f33f20
Nghi Huynh <thanhnghihuyn@gmail.com>
2024-10-23 17:18:40 +0200

    run evaluate splitchr

--- script_run_evaluate_splitchr_pheno1.sh
+++ script_run_evaluate_splitchr_pheno1.sh
@@ -2,13 +2,13 @@
 #python run_evaluate_model.py --best_param ./best_params/splitchr_pheno1_best_param_mlpmixer17_nonoverlap.json --data_dir ./ --dataset 1 --embedding_type kmer_nonoverlap --model_type Chromosomesplit --model_name MLPMixer --gpucuda 1 > ./data/results/prof_perf/prof_splitchr_pheno1_kmernonoverlap_mlpmixer.txt
 
 echo "Profiling Perf: CNN_Transformer Splitchr Pheno1"
-python run_evaluate_model.py --best_param ./best_params/splitchr_pheno1_best_param_testcnntransformer50_nonoverlap.json --data_dir ./ --dataset 1 --embedding_type kmer_nonoverlap --model_type Chromosomesplit --model_name Test1_CNN --gpucuda 1 > ./data/results/prof_perf/prof_splitchr_pheno1_kmernonoverlap_cnntransf_1.txt
+/home/di35hef/miniconda3/bin/python run_evaluate_model.py --best_param ./best_params/splitchr_pheno1_best_param_testcnntransformer50_nonoverlap.json --data_dir ./ --dataset 1 --embedding_type kmer_nonoverlap --model_type Chromosomesplit --model_name Test1_CNN --gpucuda 1 > ./data/results/prof_perf/prof_splitchr_pheno1_kmernonoverlap_cnntransf_1.txt
 
 echo "Profiling Perf: Test1_Transformer Splitchr Pheno1"
-python run_evaluate_model.py --best_param ./best_params/splitchr_pheno1_best_param_transformer54_nonoverlap.json --data_dir ./ --dataset 1 --embedding_type kmer_nonoverlap --model_type Chromosomesplit --model_name Transformer --gpucuda 1 > ./data/results/prof_perf/prof_splitchr_pheno1_kmernonoverlap_transf_1.txt
+/home/di35hef/miniconda3/bin/python run_evaluate_model.py --best_param ./best_params/splitchr_pheno1_best_param_transformer54_nonoverlap.json --data_dir ./ --dataset 1 --embedding_type kmer_nonoverlap --model_type Chromosomesplit --model_name Transformer --gpucuda 1 > ./data/results/prof_perf/prof_splitchr_pheno1_kmernonoverlap_transf_1.txt
 
 echo "Profiling Perf: Infinite_Transformer Splitchr Pheno1"
-python run_evaluate_model.py --best_param ./best_params/splitchr_pheno1_best_param_test30_infinitetransformer_nonoverlap.json --data_dir ./ --dataset 1 --embedding_type kmer_nonoverlap --model_type Chromosomesplit --model_name Test2_Infinite_Transformer --gpucuda 1 > ./data/results/prof_perf/prof_splitchr_pheno1_kmernonoverlap_inftransf_1.txt
+/home/di35hef/miniconda3/bin/python run_evaluate_model.py --best_param ./best_params/splitchr_pheno1_best_param_test30_infinitetransformer_nonoverlap.json --data_dir ./ --dataset 1 --embedding_type kmer_nonoverlap --model_type Chromosomesplit --model_name Test2_Infinite_Transformer --gpucuda 1 > ./data/results/prof_perf/prof_splitchr_pheno1_kmernonoverlap_inftransf_1.txt
 
 echo "Profiling Perf: HyperMixer Splitchr Pheno1"
-python run_evaluate_model.py --best_param ./best_params/splitchr_pheno1_best_param_hypermixer128_nonoverlap.json --data_dir ./ --dataset 1 --embedding_type kmer_nonoverlap --model_type Chromosomesplit --model_name HyperMixer --gpucuda 1 > ./data/results/prof_perf/prof_splitchr_pheno1_kmernonoverlap_hypermixer_1.txt
+/home/di35hef/miniconda3/bin/python run_evaluate_model.py --best_param ./best_params/splitchr_pheno1_best_param_hypermixer128_nonoverlap.json --data_dir ./ --dataset 1 --embedding_type kmer_nonoverlap --model_type Chromosomesplit --model_name HyperMixer --gpucuda 1 > ./data/results/prof_perf/prof_splitchr_pheno1_kmernonoverlap_hypermixer_1.txt
